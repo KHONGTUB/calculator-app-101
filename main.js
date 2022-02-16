dfef023a924366990c07d4f845ca9a984f5f72ce
KHONGTUB <jkojeda12@gmail.com>
2022-02-15 20:06:10 -0600

    Finished calculator app

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ let operation = null
 
 // this function takes in the number you type in the input field and saves it to the "firstNum" variable
 const saveFirstNumber = (num) => {
-  firstNum = parseInt(num)  
+  if (firstNum === null){
+    firstNum = parseInt(num) 
+  } else {
+    saveSecondNumber(num)
+  }
+   
 }
 
 // this function takes in the number you type in the 2nd input field and saves it to the "secondNum" variable
 const saveSecondNumber = (num) => {
   // "parseInt" is a built in function in JS that converts a string/word into a number
-  secondNum = parseInt(num)
+  if( secondNum === null)
+  {
+    secondNum = parseInt(num)
+  }
+    
 }
 
 // this function takes in two argument/numbers and returns the sum of them
@@ -30,15 +39,35 @@ const subtract = (numA, numB) => {
 // These variables are already defined but that don't point to functions. It's up to you to build the functions to complete your calculator use:
 
 const multiply = (numA, numB) => {
+
+  const product = numA * numB
+  return product
   // * to get a product then return it
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
   console.log(numA, numB)
 }
 
-const divide = null
-// / to get a quotient,
+const divide = (numA, numB) => {
 
-const modulus = null
+  if (numB === 0){
+    const quotient = "You can't divide by zero"
+    console.log(quotient)
+    return quotient
+  }
+  else{
+    const quotient = numA / numB
+    console.log(quotient)
+    return quotient
+  }
+
+}
+
+const modulus = (numA, numB) => {
+
+  const remainder = numA % numB
+  return remainder
+
+}
 // and % to get a remainder.
 
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
@@ -51,26 +80,29 @@ const changeOperation = (chosenOperation) => {
 // In order to show the user their results we have to access the DOM and stick in the value
 const putResultInElement = (operationResults) => {
   // access the DOM by writing "document" then use the method "getElementById" and pass it the id, "result".
-  document.getElementById("result").innerHTML = "Results: " + operationResults
+  document.getElementById("display").innerHTML = firstNum + " " + operation + " " + secondNum  +  " = " + operationResults
 
   // Remember, each element has built in properties like "innerHTML" which we can change to anything we like. 
   // Here we give it a string: "Results: " and add the value of the operation to it.
 }
 
+const reset = () =>{
+  document.getElementById("display").innerHTML = "0"
+}
+
 // The function uses the value of "operation" variable to determine which operation function it should use on the number: add, subtract, multiply, divide, or modulus
 const equals = () => {
   switch (operation) {
-    case "addition":  putResultInElement(add(firstNum, secondNum)) 
+    case "+":  putResultInElement(add(firstNum, secondNum)) 
     break;
-    case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
+    case "-": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "*": putResultInElement(multiply(firstNum, secondNum))
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "/": putResultInElement(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "%": putResultInElement(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
 }
-
